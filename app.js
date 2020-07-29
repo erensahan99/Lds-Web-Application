845f5cc9756f0e0fe850a3a6d815e245f3272585
erensahan99 <eren.sahan99@gmail.com>
2020-07-30 00:39:40 +0300

    added new express project structure

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-var http = require('http');
-var soap = require('soap');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var soapservice = {
-    Hello_Service: {
-        Hello_Port: {
-            // This is how to define an asynchronous function.
-            sayHello: function (args, callback) {
-                // do some work
-                console.log('sayHello: '+JSON.stringify(args));
-                callback({'greeting': 'Hello '+args.firstName.$value});
-            }
-        }
-    }
-};
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-var wsdlxml = require('fs').readFileSync('soapservice.wsdl', 'utf8'),
-    server = http.createServer(function (request, response) {
-        response.end("404: Not Found: " + request.url);
-    });
+var app = express();
 
-var PORT = process.env.PORT || 3000;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-server.listen(PORT);
-console.log('server running on port ' + PORT);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-soap.listen(server, '/soapservice', soapservice, wsdlxml);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
