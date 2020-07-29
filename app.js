158a84f5739097ec0c42b6e04f4fa54180cf198d
erensahan99 <eren.sahan99@gmail.com>
2020-07-29 09:02:28 +0300

    added new page

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express()
 
 port = process.env.PORT || 3000
 
-app.get('/',(req,res) => res.send('My Rest API running on port ' + port));
+app.get('/',(req,res) => res.send('My Rest API running on port ' + port)); 
+
+app.get('/page2',function(req,res) {
+  res.sendFile(__dirname + '/express/index.html');
+  });
 
 app.listen(port, () => {
     console.log('My Rest API running on port ' + port);
