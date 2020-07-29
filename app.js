8d877cedcfd13650a3a778dbbd041bdafbba71a2
erensahan99 <eren.sahan99@gmail.com>
2020-07-30 01:57:13 +0300

    some changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -39,3 +40,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
