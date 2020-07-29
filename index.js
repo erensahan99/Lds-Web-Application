b5d583eb5322a36827f2dea5677df3e33b9fe71b
erensahan99 <eren.sahan99@gmail.com>
2020-07-29 08:46:15 +0300

    fixed: port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ port = process.env.PORT || 3000
 
 app.get('/',(req,res) => res.send('My Rest API running on port ' + port));
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('My Rest API running on port ' + port);
 })
