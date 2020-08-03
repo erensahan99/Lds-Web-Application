44e52bfd43ed2044e2b690fe721a49655728254c
erensahan99 <eren.sahan99@gmail.com>
2020-08-03 13:03:45 +0300

    added fav icon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var soapRouter = require('./routes/soap');
@@ -13,7 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use(favicon(__dirname + '/public/images/icon.png'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
