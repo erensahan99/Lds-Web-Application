62f0a0a5a5d37ae70b7aee2184d0a26230c924b2
erensahan99 <eren.sahan99@gmail.com>
2020-08-07 09:25:36 +0300

    Added configuration file for sequelize and sequelize-cli

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "development": {
+    "username": "express-dbuser",
+    "password": "123456",
+    "database": "lodos_web_app",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port": 3306
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
